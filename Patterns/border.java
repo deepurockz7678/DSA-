7804eb7a0bcc4372243c1658f16b446b412375a0
Deepak Puvvala <pdeepakpuvvala14122004@gmail.com>
2026-08-14 21:02:55 +0530

    done

--- Patterns/border.java
+++ Patterns/border.java
@@ -2,15 +2,30 @@ package Patterns;
 
 public class border {
     public static void main (String [] args) {
-        int n = 4
-        
-         ; 
+        int n = 4; 
+        /* 
         for(int i = 0 ; i < 2*n - 1 ; i++) {
             for(int j = 0 ; j < 2*n - 1 ; j++){
                 int val = n -  Math.min(Math.min(i , j ) , Math.min(2*n - 2- i , 2*n - 2 - j));
                     System.out.print(val +" ");
             }
             System.out.println(" ");
+        }
+        */
+
+        for (int i = 0; i < 2 * n - 1; i++) {
+
+            for (int j = 0; j < 2 * n - 1; j++) {
+
+                int val = Math.max(
+                    Math.abs(n - 1 - i),
+                    Math.abs(n - 1 - j)
+                ) + 1;
+
+                System.out.print(val + " ");
+            }
+
+            System.out.println();
         }
     }
 }
