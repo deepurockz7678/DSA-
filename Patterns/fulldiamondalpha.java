814fa3adcc6e1a5efeadb6c1699746b886c3b647
Deepak Puvvala <pdeepakpuvvala14122004@gmail.com>
2026-08-14 21:18:34 +0530

    done

--- Patterns/fulldiamondalpha.java
+++ Patterns/fulldiamondalpha.java
@@ -1,5 +1,6 @@
 package Patterns;
 
+
 public class fulldiamondalpha {
     public static void main (String [] args) {
         int n = 5 ; 
@@ -8,10 +9,22 @@ public class fulldiamondalpha {
             for (int j = 1 ; j <= n - i ; j++) {
                 System.out.print("  ");
             }
-            for(char c = 'A' ; c < i ; c++ ) {
-                System.out.print(c);
-
+            for(char c = 'A' ; c < 'A' + i ; c++ ) {
+                System.out.print(c +"  ");
             }
+            System.out.println();
         }
+        for(int i = n - 1 ; i >=1; i--) {
+            //space 
+            for(int j = 1 ; j <= n - i ; j++) 
+                System.out.print("  ");
+            
+            for(char c = 'A' ; c < 'A' + i ; c++) {
+                System.out.print(c +"  ");
+            }
+            System.out.println();
+        }
+
+        }       
     }
-}
+
