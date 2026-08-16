1d07b51165d0672924cbd6fd86c5f94231ccbb98
Deepak Puvvala <pdeepakpuvvala14122004@gmail.com>
2026-08-16 09:36:27 +0530

    factors

---
+++ Fundamentals/factors.java
@@ -0,0 +1,13 @@
+package Fundamentals;
+
+public class factors {
+    public static void main(String[] args) {
+        int num = 28;
+        System.out.print("Factors of " + num + " are: ");
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
